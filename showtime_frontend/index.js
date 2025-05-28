83883e3f8f98efb75706bd8b79ac250b3d7a2c34
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-29 02:28:56 +0530

    added a react native app

--- showtime_frontend/index.js
+++ showtime_frontend/index.js
@@ -1,14 +1,9 @@
-import 'react-native-gesture-handler';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { AppRegistry } from 'react-native';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
-import React from 'react';
+import {name as appName} from './app.json';
 
-const RootApp = () => (
-  <GestureHandlerRootView style={{ flex: 1 }}>
-    <App />
-  </GestureHandlerRootView>
-);
-
-AppRegistry.registerComponent(appName, () => RootApp);
+AppRegistry.registerComponent(appName, () => App);
